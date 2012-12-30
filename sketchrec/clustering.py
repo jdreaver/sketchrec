403cc2bdc02955bf9515425f1f15491c25c85aa1
John David Reaver <jdreaver@adlerhorst.com>
2012-12-29 19:47:39 -0800

    Trying to figure clustering out

--- sketchrec/clustering.py
+++ sketchrec/clustering.py
@@ -4,23 +4,8 @@ of templates.
 """
 
 import numpy as np
-from sketchrec.image_template import stack_distance_maps
+from sketchrec.image_template import distance_matrix
 
-def distance_matrix_1(items, distance_fn):
-    N = len(items)
-    distances = np.zeros((N,N))
-    for i in range(N):
-        for j in range(i + 1, N):
-            d = distance_fn(items[i], items[j])
-            distances[i,j] = distances[j,i] = d
-    return distances
 
-def distance_matrix_3(items, distance_fn):
-    N = len(items)
-    distances = np.zeros((N, N))
-    for i in range(N):
-        row = np.array(([0]*(i+1) + [distance_fn(items[i], items[j])
-                                     for j in range(i+1, N)]))
-        distances[i] += row
-    return distances + distances.T - np.diag(distances)
+
 
